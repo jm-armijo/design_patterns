073bf88206cedec6f6afd922a56280ffad3d465a
Jose Miguel Armijo <jose.miguel.armijo@sgdigital.com>
2018-07-17 08:14:42 +1000

    Completed implementation

--- chain_of_responsibility/main.cpp
+++ chain_of_responsibility/main.cpp
@@ -1,50 +1,20 @@
-#include <iostream>
-
-class Handler
-{
-public:
-    Handler(Handler *next = nullptr) : m_next(next) {
-        std::cout << "Constructor handler" << std::endl;
-    }
-
-    void Handle() {
-        std::cout << "Handler..." << std::endl;
-        if (m_next != nullptr) {
-            std::cout << "Calling parent" << std::endl;
-            m_next->Handle();
-        }
-    }
-private:
-    Handler *m_next;
-};
-
-
-class Layer1 : public Handler
-{
-public:
-    Layer1(Handler* handler) {
-        std::cout << "Constructing Layer 1" << std::endl;
-    }
-};
-
-class Layer2 : public Handler
-{
-public:
-    Layer2(Handler* handler) : Handler(handler) {
-        std::cout << "Constructing Layer 2" << std::endl;
-    }
-};
-
-class Layer3 : public Handler
-{
-
-};
-
+#include "Layer.hpp"
 
 int main()
 {
-    auto l2 = new Layer2(nullptr);
+    auto l3 = new Layer3(nullptr);
+    auto l2 = new Layer2(l3);
     auto l1 = new Layer1(l2);
-    l1->Handle();
+
+    l1->Handle(Code::code_1);
+	std::cout << std::endl;
+
+    l1->Handle(Code::code_2);
+	std::cout << std::endl;
+    
+	l1->Handle(Code::code_3);
+	std::cout << std::endl;
+    
+	l1->Handle(Code::code_4);
     return 0;
 }
